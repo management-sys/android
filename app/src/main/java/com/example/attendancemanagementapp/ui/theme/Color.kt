5de0c6398601dbc0def3adad8aecb011d8159c11
gabeen <rkqls4764@naver.com>
2025-09-15 11:24:01 +0900

    style: 색상 변수 추가

---
+++ app/src/main/java/com/example/attendancemanagementapp/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.example.attendancemanagementapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val MainBlue = Color(0xFF3C7CED)
+val MiddleBlue = Color(0xFF5583D5)
+val LightBlue = Color(0xFFE6EEFA)
+
+val DarkGray = Color(0xFF656565)
+val TextGray = Color(0xFF828282)
+val DisableGray = Color(0xFFD8D8D8)
+val LightGray = Color(0xFFEFEFEF)
