c4ecfcf732c3b68a8601c15b698ca00340d562fe
gabeen <rkqls4764@naver.com>
2025-09-15 11:22:45 +0900

    feat: 기본 디알로그 컴포넌트 생성
    기본 디알로그 컴포넌트 생성

---
+++ app/src/main/java/com/example/attendancemanagementapp/view/component/BasicDialog.kt
@@ -0,0 +1,67 @@
+package com.example.attendancemanagementapp.view.component
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.attendancemanagementapp.ui.theme.DarkGray
+import com.example.attendancemanagementapp.ui.theme.MainBlue
+
+/* 기본 디알로그 */
+@Composable
+fun BasicDialog(
+    title: String,              // 제목
+    text: String = "",          // 내용
+    onDismiss: () -> Unit,
+    onClickConfirm: () -> Unit
+) {
+    AlertDialog(
+        title = {
+            Text(
+                text = title,
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        text = {
+            Text(
+                text = text,
+                fontSize = 15.sp
+            )
+        },
+        onDismissRequest = {
+            onDismiss()
+        },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onClickConfirm()
+                    onDismiss()
+                }
+            ) {
+                Text(
+                    text = "삭제",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Red
+                )
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = {
+                    onDismiss()
+                }
+            ) {
+                Text(
+                    text = "취소",
+                    fontSize = 16.sp,
+                    color = MainBlue
+                )
+            }
+        }
+    )
+}
