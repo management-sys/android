f7958e35fdafaf285e4010da57f16b5c6be25f7b
rkqls4764 <rkqls4764@naver.com>
2025-09-15 15:50:15 +0900

    feat: 공통코드 관리 viewmodel에 전체 공통코드 조회 함수 추가

--- app/src/main/java/com/example/attendancemanagementapp/ui/commoncode/CodeViewModel.kt
+++ app/src/main/java/com/example/attendancemanagementapp/ui/commoncode/CodeViewModel.kt
@@ -1,11 +1,18 @@
-package com.example.attendancemanagementapp.viewmodel
+package com.example.attendancemanagementapp.ui.commoncode
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.example.attendancemanagementapp.model.CodeDTO
+import androidx.lifecycle.viewModelScope
+import com.example.attendancemanagementapp.data.repository.CommonCodeRepository
+import com.example.attendancemanagementapp.ui.commoncode.add.CodeAddUiState
+import com.example.attendancemanagementapp.ui.commoncode.detail.CodeDetailUiState
+import com.example.attendancemanagementapp.ui.commoncode.edit.CodeEditUiState
+import com.example.attendancemanagementapp.ui.commoncode.list.CodeListUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 data class FieldState(
@@ -13,34 +20,17 @@ data class FieldState(
     val error: String? = null
 )
 
-data class CodeManageUiState(
-    val codes: List<CodeDTO.CodeListInfo> = emptyList(),        // 공통코드 목록
-    val selectedCode: String = "",                              // 선택한 공통코드의 코드
-    val searchText: FieldState = FieldState(),                  // 검색어
-    val selectedFilter: FieldState = FieldState(value = "전체")   // 선택한 필터
-)
-
-data class CodeDetailUiState(
-    val codeInfo: CodeDTO.CodeInfo = CodeDTO.CodeInfo() // 공통코드 정보
-)
-
-data class CodeEditUiState(
-    val codeName: FieldState = FieldState(),    // 코드명
-    val codeValue: FieldState = FieldState(),   // 코드값
-    val description: FieldState = FieldState()  // 설명
-)
-
-data class CodeAddUiState(
-    val inputData: CodeDTO.CodeInfo = CodeDTO.CodeInfo() // 입력한 데이터
-)
-
 enum class SearchField { SEARCHTEXT, FILTER }
 enum class CodeInfoField { CODENAME, CODEVALUE, DESCRIPTION }
 
 @HiltViewModel
-class CodeViewModel @Inject constructor() : ViewModel() {
-    private val _codeManageUiState = MutableStateFlow(CodeManageUiState())
-    val codeManageUiState = _codeManageUiState.asStateFlow()
+class CodeViewModel @Inject constructor(private val repository: CommonCodeRepository) : ViewModel() {
+    companion object {
+        private const val TAG = "CodeViewModel"
+    }
+
+    private val _codeListUiState = MutableStateFlow(CodeListUiState())
+    val codeListUiState = _codeListUiState.asStateFlow()
     private val _codeDetailUiState = MutableStateFlow(CodeDetailUiState())
     val codeDetailUiState = _codeDetailUiState.asStateFlow()
     private val _codeEditUiState = MutableStateFlow(CodeEditUiState())
@@ -56,10 +46,10 @@ class CodeViewModel @Inject constructor() : ViewModel() {
     /* 검색 필드 값 변경 */
     fun onSearchFieldChange(field: SearchField, input: String) {
         when (field) {
-            SearchField.SEARCHTEXT -> _codeManageUiState.update { state ->
+            SearchField.SEARCHTEXT -> _codeListUiState.update { state ->
                 state.copy(searchText = state.searchText.copy(value = input))
             }
-            SearchField.FILTER -> _codeManageUiState.update { state ->
+            SearchField.FILTER -> _codeListUiState.update { state ->
                 state.copy(selectedFilter = state.selectedFilter.copy(value = input))
             }
         }
@@ -80,13 +70,24 @@ class CodeViewModel @Inject constructor() : ViewModel() {
         }
     }
 
-    /* 공통코드 검색 */
-    fun getFilteredCode() {
-
+    /* 전체 공통코드 조회 */
+    fun getCodes() {
+        viewModelScope.launch {
+            repository.getCommonCodes().collect() { result ->
+                result
+                    .onSuccess { commonCodes ->
+                        _codeListUiState.update { it.copy(codes = commonCodes) }
+                        Log.d(TAG, "전체 공통코드 조회 성공\n${commonCodes}")
+                    }
+                    .onFailure { e ->
+                        e.printStackTrace()
+                    }
+            }
+        }
     }
 
-    /* 공통코드 목록 조회 */
-    fun getCodes() {
+    /* 공통코드 검색 */
+    fun getFilteredCode() {
 
     }
 
