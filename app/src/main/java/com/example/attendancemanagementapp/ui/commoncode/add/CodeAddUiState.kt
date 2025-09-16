5636309b50985b9fb0a01a5db9f11866ce544c42
rkqls4764 <rkqls4764@naver.com>
2025-09-16 09:32:16 +0900

    feat: 공통코드 등록 화면 UiState 생성

--- app/src/main/java/com/example/attendancemanagementapp/ui/commoncode/add/CodeAddUiState.kt
+++ app/src/main/java/com/example/attendancemanagementapp/ui/commoncode/add/CodeAddUiState.kt
@@ -1,7 +1,11 @@
 package com.example.attendancemanagementapp.ui.commoncode.add
 
 import com.example.attendancemanagementapp.data.dto.CommonCodeDTO
+import com.example.attendancemanagementapp.ui.commoncode.FieldState
 
 data class CodeAddUiState(
-    val inputData: CommonCodeDTO.CodeInfo = CommonCodeDTO.CodeInfo() // 입력한 데이터
+    val inputData: CommonCodeDTO.CodeInfo = CommonCodeDTO.CodeInfo(),   // 입력한 데이터
+    val selectedCode: String = "",                                      // 선택한 공통코드의 코드
+    val searchText: FieldState = FieldState(),                          // 검색어
+    val selectedFilter: FieldState = FieldState(value = "전체")          // 선택한 필터
 )
