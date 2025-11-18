1b76999ddc8641b44fad40d70fb88b409be27506
rkqls4764 <rkqls4764@naver.com>
2025-11-18 14:44:08 +0900

    feat: 모든 화면 스택 지우고 특정 화면으로 이동 이벤트 추가

--- app/src/main/java/com/example/attendancemanagementapp/ui/base/UiEffect.kt
+++ app/src/main/java/com/example/attendancemanagementapp/ui/base/UiEffect.kt
@@ -3,5 +3,6 @@ package com.example.attendancemanagementapp.ui.base
 sealed interface UiEffect {
     data object NavigateBack: UiEffect  // 뒤로 가기
     data class Navigate(val route: String): UiEffect    // 특정 화면으로 이동
+    data class AllDeleteNavigate(val route: String): UiEffect   // 이전 스택 모두 지우고 특정 화면으로 이동
     data class ShowToast(val message: String): UiEffect // 토스트 출력
 }
